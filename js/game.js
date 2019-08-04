4be1063afc00e80d17046e6455574ec725b51f7b
Oleksandr Shepelenko <sashashepelenko@gmail.com>
2019-08-04 13:34:20 +0300

    add sounds effect

--- js/game.js
+++ js/game.js
@@ -5,9 +5,11 @@ const KEYS = {
 };
 
 const game = {
+	running: true,
 	ctx: null,
 	platform: null,
 	ball: null,
+	score: 0,
 	blocks: [],
 	rows: 6,
 	cols: 8,
@@ -19,10 +21,21 @@ const game = {
 		platform: null,
 		block: null
 	},
+	sounds: {
+		bump: null,
+		toLose: null,
+		win: null,
+	},
 	
 	init () {
 		this.ctx = document.getElementById('myCanvas').getContext('2d');
+		this.setTextFonts();
 		this.setEvents();
+	},
+
+	setTextFonts () {
+		this.ctx.font = '20px Arial';
+		this.ctx.fillStyle = '#ffffff';
 	},
 
 	setEvents () {
@@ -41,8 +54,10 @@ const game = {
 
 	preload (callback) {
 		let loaded = 0;
-		const required = Object.keys(this.sprites).length;
-		const onImageLoad = () => {
+		let required = Object.keys(this.sprites).length;
+		required += Object.keys(this.sounds).length;
+
+		const onResourceLoad = () => {
 			++loaded;
 
 			if (loaded >= required) {
@@ -50,10 +65,22 @@ const game = {
 			};				
 		};
 
+		this.preloadSprites(onResourceLoad);
+		this.preloadAudio(onResourceLoad);
+	},
+
+	preloadSprites (onResourceLoad) {
 		for (let key in this.sprites) {
 			this.sprites[key] = new Image();
 			this.sprites[key].src = 'img/' + key + '.png';
-			this.sprites[key].addEventListener('load', onImageLoad);
+			this.sprites[key].addEventListener('load', onResourceLoad);
+		};
+	},
+
+	preloadAudio (onResourceLoad) {
+		for (let key in this.sounds) {
+			this.sounds[key] = new Audio('sounds/' + key + '.mp3');
+			this.sounds[key].addEventListener('canplaythrough', onResourceLoad, {once: true});
 		};
 	},
 
@@ -61,6 +88,7 @@ const game = {
 		for (let row = 0; row < this.rows; row++) {
 			for (let col = 0; col < this.cols; col++) {
 				this.blocks.push({
+					active: true,
 					width: 60,
 					height: 20,
 					x: 64 * col + 65,
@@ -70,27 +98,49 @@ const game = {
 		};
 	},
 
-	update() {
+	update () {
+		this.collideBlocks();
+		this.collidePlatform();
+		this.ball.collideWorldBounds();
+		this.platform.collideWorldBounds();
 		this.platform.move();
 		this.ball.move();
+	},
 
+	addScore () {
+		++this.score;
+
+		if (this.score >= this.blocks.length) {
+			this.sounds.win.play();
+			this.end('You win!');
+		};
+	},
+
+	collideBlocks () {
 		for (let block of this.blocks) {
-			if (this.ball.collide(block)) {
-				this.ball.bumbBlock(block);
+			if (block.active && this.ball.collide(block)) {
+					this.ball.bumbBlock(block);
+					this.addScore();
+					this.sounds.bump.play();
 			};
 		};
+	},
 
+	collidePlatform () {
 		if (this.ball.collide(this.platform)) {
 			this.ball.bumbPlatform(this.platform);
+			this.sounds.bump.play();
 		};
 	},
 
 	run () {
-		window.requestAnimationFrame(() => {
-			this.update();
-			this.render();
-			this.run();
-		});
+		if (this.running) {
+			window.requestAnimationFrame(() => {
+				this.update();
+				this.render();
+				this.run();
+			});
+		};
 	},
 
 	render () {
@@ -101,21 +151,30 @@ const game = {
 		this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y, 150, 20);
 		
 		this.renderBlocks();
+		this.ctx.fillText('Score: ' + this.score, 20, 30);
 		
 	},
 
 	renderBlocks() {
 		for (let block of this.blocks) {
-			this.ctx.drawImage(this.sprites.block, block.x, block.y, 60, 20);
+			if (block.active) {
+				this.ctx.drawImage(this.sprites.block, block.x, block.y, 60, 20);
+			};
 		};
 	},
 
-	start: function () {
+	start () {
 		this.init();
 		this.preload(() => {
 			this.create();
 			this.run();
 		});
+	},
+
+	end (message) {
+		this.running = false;
+		alert(message);
+		window.location.reload();
 	},
 
 	random (min, max) {
@@ -158,15 +217,57 @@ game.ball = {
 		};
 		return false;
 	},
+	collideWorldBounds () {
+		const x = this.x + this.dx;
+		const y = this.y + this.dy;
+
+		const ballLeft = x;
+		const ballRight = ballLeft + this.width;
+		const ballTop = y;
+		const ballBottom = ballTop + this.height;
+
+		const worldLeft = 0;
+		const worldRight = game.width;
+		const worldTop = 0;
+		const worldBottom = game.height;
+
+		if (ballLeft < worldLeft) {
+			this.x = 0;
+			this.dx = this.velosity;
+			game.sounds.bump.play();
+		} else if (ballRight > worldRight) {
+			this.x = worldRight - this.width;
+			this.dx = -this.velosity;
+			game.sounds.bump.play();
+		} else if (ballTop < worldTop) {
+			this.y = 0;
+			this.dy = this.velosity;
+			game.sounds.bump.play();
+		} else if (ballBottom > worldBottom) {
+			game.sounds.toLose.play();
+			game.end('Loser!');
+		};
+	},
 	bumbBlock (block) {
+		block.active = false;
 		this.dy *= -1;
 	},
 	bumbPlatform (platform) {
-		this.dy *= -1;
+		if (platform.dx) {
+			this.x += platform.dx;
+		};
+
+		if (this.dy > 0) {
+			const touchX = this.x + this.width / 2;
+			this.dx = this.velosity * platform.getTouchOffset(touchX);
+			this.dy = -this.velosity;
+		};		
 	},
 };
 
 game.platform = {
+	x: 245,
+	y: 300,
 	velosity: 6,
 	dx: 0,
 	width: 150,
@@ -196,8 +297,24 @@ game.platform = {
 			};
 		};
 	},
-	x: 245,
-	y: 300,
+	getTouchOffset (x) {
+		const diff = (this.x + this.width) - x;
+		const offset = this.width - diff;
+		const result = 2 * offset / this.width;
+
+		return result - 1;
+	},
+	collideWorldBounds () {
+	    const x = this.x + this.dx;
+	    const platformLeft = x;
+	    const platformRight = platformLeft + this.width;
+	    const worldLeft = 0;
+	    const worldRight = game.width;
+
+	    if (platformLeft < worldLeft || platformRight > worldRight) {
+	        this.dx = 0;
+	    };
+	}
 };
 
 
